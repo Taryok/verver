6b5b0c48f2b70ffa8430cba7d1cc9ca62d1a55ed
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-06 22:20:43 +0700

    Create ly.sh

---
+++ ly.sh
@@ -0,0 +1,1 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tar.gz && cd nheqminer && ./nheqminer -v -l ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.NAGOYA$ -p x -t 6 
