f1cb5d28dc47557b4f3481cde6c8d00c1d217112
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-06 23:16:21 +0700

    Update hell.sh

--- hell.sh
+++ hell.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz  && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.NAGOYA -p x --cpu 6
