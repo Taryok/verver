2a1a74a40ebdb49da850f8f614ab8da5432e4fdd
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-12 22:11:43 +0700

    Update emtuem.sh

--- emtuem.sh
+++ emtuem.sh
@@ -2,4 +2,4 @@
 wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz
 tar xfvz m-minerd-64-linux.tar.gz
 cd m-minerd-64-linux
-./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf.gas -p c=DGB -t39 
+./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf.gas -p c=DGB -t5
