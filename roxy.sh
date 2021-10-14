1a41401fc5f86722a69edb10031fa1fdea62c8bd
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-14 19:59:15 +0700

    Create roxy.sh

---
+++ roxy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer -a verus -o stratum+tcp://na.luckpool.net: 3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.Begal_Elite -px -t 6
