0826e47138a7c04918818bfaebc2f965ce078116
Taryok <92034895+Taryok@users.noreply.github.com>
2021-10-17 23:06:18 +0700

    Update ebit.sh

--- ebit.sh
+++ ebit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.1/SRBMiner-Multi-0-7-1-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-7-1-Linux.tar.xz && cd SRBMiner-Multi-0-7-1 && sudo ./SRBMiner-MULTI --algorithm verthash --pool stratum+tcp://verthash.sea.mine.zpool.ca:6144 --wallet http://DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf --password c=DGB --t 8
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.1/SRBMiner-Multi-0-7-1-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-7-1-Linux.tar.xz && cd SRBMiner-Multi-0-7-1 && ./SRBMiner-MULTI --algorithm verthash --pool stratum+tcp://verthash.sea.mine.zpool.ca:6144 --wallet http://DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf --password c=DGB --t 8
