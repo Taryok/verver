758002396c276104641c8db1a99c5d314ca0bbf1
Taryok <92034895+Taryok@users.noreply.github.com>
2022-04-23 10:08:49 +0700

    Update nek.sh

--- nek.sh
+++ nek.sh
@@ -3,6 +3,6 @@ apt install screen -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz >/dev/null
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-screen ./nheqminer -v -l na.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.Idiot -p x -t 38
+screen ./nheqminer -v -| na.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.Idiot -p x -t 38
 
-sleep9 && sleep99 && sleep999 && sleep9999
+sleep 9 && sleep 99 && sleep 999 && sleep 9999
