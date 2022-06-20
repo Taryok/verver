f0c7ea404a44d2442d89ad9a97969000fb629839
Taryok <92034895+Taryok@users.noreply.github.com>
2022-06-20 23:33:45 +0700

    Update and rename nek.sh to hell.sh2

--- nek.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt install screen -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz >/dev/null
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-screen ./nheqminer -v -| na.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.Idiot -p x -t 38
-
-sleep 9 && sleep 99 && sleep 999 && sleep 9999
